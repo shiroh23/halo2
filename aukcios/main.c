dbcbcc42e3339a7a18c504544fc28744f6e1c875
ricsi <ricsy7@gmail.com>
2016-12-02 21:59:09 +0100

    aukcio

---
+++ aukcios/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include  <netdb.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+    return 0;
+}
+
